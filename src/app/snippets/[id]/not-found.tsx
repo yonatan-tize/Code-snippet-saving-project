9b32a71263d2fd8385676151b6293be05bb34bee
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-12-25 23:26:04 +0300

    feat: not found page

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -1,7 +1,8 @@
 export default function SnippetNotFound(){
   return (
     <div>
-      <h1 className="text-gray-800 font-bold text-xl">The snippet you are looking does not exist.</h1>
+      <h1 className="text-gray-800 font-bold text-xl">
+        The snippet you are looking does not exist.</h1>
     </div>
   )
 } 
